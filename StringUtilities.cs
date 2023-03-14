b252b51c3757bb70bfc381623c23687775324f02
Antyss77 <fragnatiikpvpbest@gmail.com>
2023-03-14 14:19:45 +0100

    IsPalindrome - Allows to know if a word is reversible example "racecar". CountLetters - Allows you to count the letters RemoveDuplicateCharacters - Allows you to delete a duplicate character ConvertToCamelCase - Allows to convert a string to camelCase

--- StringUtilities.cs
+++ StringUtilities.cs
@@ -107,6 +107,56 @@ namespace Strings {
             Array.Reverse(words);
             return string.Join(" ", words);
         }
+
+        // Determines if the given string is a palindrome
+        public static bool IsPalindrome(string input) {
+            // Remove all non-letter characters and convert to lowercase
+            string cleanString = new string(input.Where(char.IsLetter).Select(char.ToLower).ToArray());
+
+            // Check if the string is equal to its reverse
+            return cleanString == new string(cleanString.Reverse().ToArray());
+        }
+
+        // Counts the number of letters in the given string
+        public static int CountLetters(string input) {
+            // Remove all non-letter characters and count the length of the resulting string
+            return input.Count(char.IsLetter);
+        }
+
+        // Removes duplicate characters from the given string
+        public static string RemoveDuplicateCharacters(string input) {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+
+            // Convert input to char array and remove duplicates
+            var chars = input.ToCharArray().Distinct().ToArray();
+
+            // Convert char array back to string
+            return new string(chars);
+        }
+
+        // Converts input string to camel case
+        public static string ConvertToCamelCase(string input) {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+
+            // Convert input to lowercase and split by whitespace or underscore
+            var words = input.ToLower().Split(new[] {' ', '_'}, StringSplitOptions.RemoveEmptyEntries);
+
+            // Capitalize first letter of each word except the first one
+            for (int i = 1; i < words.Length; i++)
+            {
+                var chars = words[i].ToCharArray();
+                chars[0] = char.ToUpper(chars[0]);
+                words[i] = new string(chars);
+            }
+
+            // Join words and return result
+            return string.Join("", words);
+        }
     }
 }
-
