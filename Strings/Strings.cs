3013c165911749499728fd189c126d3726ec96ea
Jimmy Millo <jimmy.mll@hotmail.com>
2023-03-23 14:13:00 +0100

    perfs: create `IsValidEmail` and `IsValidPhoneNumber` method overloads

--- Strings/Strings.cs
+++ Strings/Strings.cs
@@ -5,14 +5,31 @@ using PhoneNumbers;
 
 
 namespace Strings {
-    public static class Strings {
+    public static partial class Strings
+    {
+
+        /// <summary>
+        /// Represents a regular expression that can be used to validate a mail address.
+        /// </summary>
+        /// <returns>A regular expression that can be used to validate a mail address.</returns>
+        [GeneratedRegex(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")]
+        private static partial Regex MailAddressRegex();
+        
+        /// <summary>
+        /// Represents a regular expression that can be used to validate a phone number.
+        /// </summary>
+        /// <returns>A regular expression that can be used to validate a phone number.</returns>
+        [GeneratedRegex(@"^(\+?\d{1,3}[\s-]?)?\(?\d{3}\)?[\s-]?\d{3}[\s-]?\d{4}$")]
+        private static partial Regex PhoneNumberRegex();
+        
         // Removes specified characters from the given string
         public static string RemoveCharacters(this string input, char[] charactersToRemove) {
             return new string(input.Where(c => !charactersToRemove.Contains(c)).ToArray());
         }
 
         // Validates email address
-        public static bool IsValidEmail(this string email) {
+        [Obsolete("Use IsValidEmail instead")]
+        public static bool IsValidEmailOld(this string email) {
             if (string.IsNullOrEmpty(email))
             {
                 return false;
@@ -29,9 +46,20 @@ namespace Strings {
                 return false;
             }
         }
+        
+        /// <summary>
+        /// Validates the given email address.
+        /// </summary>
+        /// <param name="email">The email address to validate.</param>
+        /// <returns><c>true</c> if the given email address is valid; otherwise, <c>false</c>.</returns>
+        public static bool IsValidEmail(this string email)
+        {
+            return !string.IsNullOrEmpty(email) && MailAddressRegex().IsMatch(email);
+        }
 
         // Validates phone number
-        public static bool IsValidPhoneNumber(this string phoneNumber) {
+        [Obsolete("Use IsValidPhoneNumber instead")]
+        public static bool IsValidPhoneNumberOld(this string phoneNumber) {
             if (string.IsNullOrEmpty(phoneNumber))
             {
                 return false;
@@ -41,6 +69,16 @@ namespace Strings {
             var regex = new Regex(@"^(\+?\d{1,3}[\s-]?)?\(?\d{3}\)?[\s-]?\d{3}[\s-]?\d{4}$");
 
             return regex.IsMatch(phoneNumber);
+        }
+        
+        /// <summary>
+        /// Validates the given phone number.
+        /// </summary>
+        /// <param name="phoneNumber">The phone number to validate.</param>
+        /// <returns><c>true</c> if the given phone number is valid; otherwise, <c>false</c>.</returns>
+        public static bool IsValidPhoneNumber(this string phoneNumber)
+        {
+            return !string.IsNullOrEmpty(phoneNumber) && PhoneNumberRegex().IsMatch(phoneNumber);
         }
 
         // Counts the number of occurrences of a substring in the given string
@@ -54,7 +92,8 @@ namespace Strings {
         }
 
         // Reverses the order of words in the given string
-        public static string ReverseWords(this string input) {
+        [Obsolete("Use ReverseWords instead")]
+        public static string ReverseWordsOld(this string input) {
             if (string.IsNullOrEmpty(input))
             {
                 return input;
@@ -65,6 +104,36 @@ namespace Strings {
             return string.Join(" ", words);
         }
 
+        /// <summary>
+        /// Reverses the order of words in the given string.
+        /// </summary>
+        /// <param name="input">The input to reverse words.</param>
+        /// <returns>The input string with the order of words reversed.</returns>
+        public static string ReverseWords(this string input)
+        {
+            var span = input.AsSpan();
+            
+            var builder = new StringBuilder();
+            
+            var start = span.Length;
+            
+            for (var i = span.Length - 1; i >= 0; i--)
+            {
+                if (span[i] is not ' ')
+                    continue;
+                
+                builder
+                    .Append(span.Slice(i + 1, start - i - 1))
+                    .Append(' ');
+                
+                start = i;
+            }
+            
+            builder.Append(span[..start]);
+            
+            return builder.ToString();
+        }
+
         // Determines if the given string is a palindrome
         public static bool IsPalindrome(this string input) {
             // Remove all non-letter characters and convert to lowercase
@@ -73,7 +142,6 @@ namespace Strings {
             // Check if the string is equal to its reverse
             return cleanString == new string(cleanString.Reverse().ToArray());
         }
-
 
         // Counts the number of letters in the given string
         public static int CountLetters(this string input) {
